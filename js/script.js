c474d2e2fdc559c178b8c86c623ce6350faf7dde
0raclewind <look4award@yahoo.com>
2021-11-17 23:32:03 +0000

    Development changes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,3 @@ $('a[href*=\\#]').on('click', function (event) {
     event.preventDefault();
     $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 300);
 });
-
-// $(window).scroll((e) => {
-//     console.log($(window).scrollTop());
-// })
